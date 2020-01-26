d6117b1e478093b046cd9ba688ab5276f60dbdbb
HaniAgha <hani.agha@hotmail.com>
2020-01-26 18:45:28 -0500

    first

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ request('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=
 router.post('/',function(req, res, next) {
 
   console.log(req)
-
+  request.post('https://webhook.site/1b0bf188-7392-48df-9455-30df1be8222d', {form:{req}})
 })
 
 });
